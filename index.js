912bf0ac3fdda8d51a0c498ad53dd1e153ec41f1
Rose <74274678+ZeaZolf@users.noreply.github.com>
2025-04-11 21:14:21 -0700

    removed light/dark feature

--- index.js
+++ index.js
@@ -2,9 +2,3 @@ function getAlbum(){
     alert("Button call to function successful");
 }
 
-function changeLightMode(){
-    alert("Change the light mode");
-   /* const body = document.querySelector("body");
-    console.log(body);
-    body.style.setProperty("" */
-}
