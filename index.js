a1495abf9159a436a32f49ec3243787d609d1681
Rose <74274678+ZeaZolf@users.noreply.github.com>
2025-04-24 22:37:04 -0700

    Finished up basics

--- index.js
+++ index.js
@@ -6,14 +6,15 @@ let httpsArgs = {
     }
 }
 
-var apiKey = "Left out for GitHub. Go get your own API key!";
+var apiKey = "Get your own KEY!";
 function getAlbum(){
-    alert("Button call to function successful");
+    // alert("Button call to function successful"); for testing button, no longer needed.
     let artistName = document.getElementById("artistName").value;
     let albumName = document.getElementById("albumName").value;
     fetch("http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=" + apiKey +"&artist="+artistName + "&album=" + albumName +"&format=json", httpsArgs)
     .then(response => response.json())
     .then(data => {
+
         const container = document.querySelector("#albumContainer");
         const album = document.createElement("div");
         container.appendChild(album);
@@ -24,11 +25,23 @@ function getAlbum(){
 
         const artist = document.createElement("h3");
         artist.textContent = data.album.artist;
-
+        
+        const art = document.createElement("img");
+        art.src = data.album.image[2]["#text"];
+        
+        const trackCount = data.album.tracks.track.length;
+        
+        // console.log(trackCount); no longer needed.
+        const info = document.createElement("p");
+        info.textContent = trackCount + " tracks * " + data.album.listeners + " listeners";
+        
+        album.appendChild(art);
         album.appendChild(aName);
         album.appendChild(artist);
+        album.appendChild(info);
         container.appendChild(album);
-        console.log(data);
+        
+        // console.log(data); for debugging; no longer needed
     })
 }
 
